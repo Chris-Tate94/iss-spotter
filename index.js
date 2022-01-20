b9532101060fb3cba389e734ed958c968f7276a0
Chris Tate <chris_tate_94@hotmail.com>
2022-01-20 23:40:42 +0000

    added functions to get ip and coordinates

---
+++ index.js
@@ -0,0 +1,21 @@
+const request = require("request");
+const { fetchMyIP } = require("./iss");
+const { fetchCoordsByIP } = require("./iss");
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned IP:", ip);
+});
+
+fetchCoordsByIP("185.203.218.145", (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned coordinates:", coordinates);
+});
